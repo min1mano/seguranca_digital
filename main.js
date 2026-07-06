75fffa1e13d54a5bcf26218f418aa0982e9ce1f3
min1mano <cristo.emanuele@escola.pr.gov.br>
2026-07-06 19:57:35 +0000

    aulas 8 e 9 calculo da entropia

--- main.js
+++ main.js
@@ -58,20 +58,23 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificaSenha();
+    classificaSenha(alfabeto.length);
 }
 
-function classificaSenha(){
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
     forcaSenha.classList.remove('fraca','media','forte');
-        if (tamanhoSenha > 11){
-           forcaSenha.classList.add('forte');
-        } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
-            forcaSenha.classList.add('media');
-        } else if (tamanhoSenha <= 5){
-            forcaSenha.classList.add('fraca');
-        }
+        if (entropia > 57){
+    forcaSenha.classList.add('forte');
+} else if (entropia > 35 && entropia < 57 ) {
+    forcaSenha.classList.add('media');
+} else if (entropia <= 35){
+    forcaSenha.classList.add('fraca');
+}
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }   
-
 
 
 
